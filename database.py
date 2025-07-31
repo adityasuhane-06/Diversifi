932019064729b85b9ace1347022cb3ffdbfe882d
Aditya Suhane <adityasuhane01@gmail.com>
2025-07-31 05:57:45 +0530

    changes

--- database.py
+++ database.py
@@ -6,10 +6,11 @@ import sqlalchemy.ext.declarative as dec
 
 load_dotenv()
 
-DATABASE_URL=(
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# DATABASE_URL=(
+#     f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     
-)
+# )
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine=create_engine(DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
